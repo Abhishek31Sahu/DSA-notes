03b0c1aa2fb6e826a7824e318c584b4cd0a9aae9
Abhishek31Sahu <abhishek31102006@outlook.com>
2024-11-23 13:15:37 +0530

    Update insertingSorting.java

--- insertingSorting.java
+++ insertingSorting.java
@@ -6,13 +6,14 @@ public class insertingSorting {
         int [] arr ={6,4,8,3,68};
         
         for (int i = 1; i < arr.length; i++) {
-            int j = i;
-            while(j>0&&arr[j-1]>arr[j]){
-                int temp = arr[j-1];
-                arr[j-1] = arr[j];
-                arr[j] = temp;
+            int key = arr[i];
+            int j=i-1;
+            while(j>=0 && arr[j]>key){
+                arr[j+1]=arr[j];
                 j--;
             }
+            arr[j+1]=key;
+                
         }
     
         for(int abhi : arr){
